216e9d7eae58e1ccc6f25cca3de0aa3ce5ded090
Alex <mingzhangliu@gmail.com>
2024-06-03 21:11:25 -0400

    Initialized post

---
+++ linkedin_scraper/post.py
@@ -0,0 +1,120 @@
+import time
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from .objects import Scraper
+import os
+from linkedin_scraper import selectors
+
+
+class Person(Scraper):
+
+    __TOP_CARD = "scaffold-layout__main"
+    __WAIT_FOR_ELEMENT_TIMEOUT = 5
+
+    def __init__(
+        self,
+        linkedin_url=None,
+        name=None,
+        about=None,
+        driver=None,
+        get=True,
+        scrape=True,
+        close_on_complete=True,
+        time_to_wait_after_login=0,
+    ):
+        self.linkedin_url = linkedin_url
+        self.name = name
+        self.about = about or []
+        self.also_viewed_urls = []
+
+        if driver is None:
+            try:
+                if os.getenv("CHROMEDRIVER") == None:
+                    driver_path = os.path.join(
+                        os.path.dirname(__file__), "drivers/chromedriver"
+                    )
+                else:
+                    driver_path = os.getenv("CHROMEDRIVER")
+
+                driver = webdriver.Chrome(driver_path)
+            except:
+                driver = webdriver.Chrome()
+
+        if get:
+            driver.get(linkedin_url)
+
+        self.driver = driver
+
+        if scrape:
+            self.scrape(close_on_complete)
+
+    def scrape(self, close_on_complete=True):
+        if self.is_signed_in():
+            self.scrape_logged_in(close_on_complete=close_on_complete)
+        else:
+            print("you are not logged in!")
+
+    def _click_see_more_by_class_name(self, class_name):
+        try:
+            _ = WebDriverWait(self.driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
+                EC.presence_of_element_located((By.CLASS_NAME, class_name))
+            )
+            div = self.driver.find_element(By.CLASS_NAME, class_name)
+            div.find_element(By.TAG_NAME, "button").click()
+        except Exception as e:
+            pass
+
+    def is_open_to_work(self):
+        try:
+            return "#OPEN_TO_WORK" in self.driver.find_element(By.CLASS_NAME,"pv-top-card-profile-picture").find_element(By.TAG_NAME,"img").get_attribute("title")
+        except:
+            return False
+
+    def scrape_logged_in(self, close_on_complete=True):
+        driver = self.driver
+        duration = None
+
+        root = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
+            EC.presence_of_element_located(
+                (
+                    By.CLASS_NAME,
+                    self.__TOP_CARD,
+                )
+            )
+        )
+        self.focus()
+        self.wait(5)
+
+        # get name and location
+        self.get_name_and_location()
+
+        self.open_to_work = self.is_open_to_work()
+
+        # get about
+        self.get_about()
+        driver.execute_script(
+            "window.scrollTo(0, Math.ceil(document.body.scrollHeight/2));"
+        )
+        driver.execute_script(
+            "window.scrollTo(0, Math.ceil(document.body.scrollHeight/1.5));"
+        )
+
+        driver.get(self.linkedin_url)
+        if close_on_complete:
+            driver.quit()
+
+    @property
+    def company(self):
+            return None
+
+    @property
+    def job_title(self):
+            return None
+
+    def __repr__(self):
+        return ""
